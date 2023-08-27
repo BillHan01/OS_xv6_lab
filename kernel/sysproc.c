4111da6cec616001a0b67878f5d69b7add9f0368
韩嘉睿 <80232148+BillHan01@users.noreply.github.com>
2023-08-27 19:00:36 +0800

    copied from local repository

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -1,7 +1,8 @@
 #include "types.h"
 #include "riscv.h"
-#include "defs.h"
 #include "param.h"
+#include "defs.h"
+#include "date.h"
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
@@ -10,7 +11,8 @@ uint64
 sys_exit(void)
 {
   int n;
-  argint(0, &n);
+  if(argint(0, &n) < 0)
+    return -1;
   exit(n);
   return 0;  // not reached
 }
@@ -31,17 +33,20 @@ uint64
 sys_wait(void)
 {
   uint64 p;
-  argaddr(0, &p);
+  if(argaddr(0, &p) < 0)
+    return -1;
   return wait(p);
 }
 
 uint64
 sys_sbrk(void)
 {
-  uint64 addr;
+  int addr;
   int n;
 
-  argint(0, &n);
+  if(argint(0, &n) < 0)
+    return -1;
+  
   addr = myproc()->sz;
   if(growproc(n) < 0)
     return -1;
@@ -54,11 +59,13 @@ sys_sleep(void)
   int n;
   uint ticks0;
 
-  argint(0, &n);
+
+  if(argint(0, &n) < 0)
+    return -1;
   acquire(&tickslock);
   ticks0 = ticks;
   while(ticks - ticks0 < n){
-    if(killed(myproc())){
+    if(myproc()->killed){
       release(&tickslock);
       return -1;
     }
@@ -68,12 +75,64 @@ sys_sleep(void)
   return 0;
 }
 
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+  // lab pgtbl: your code here.
+  int num_check;
+  uint64 va;
+  uint64 dstva;
+  pagetable_t pagetable = myproc()->pagetable;
+  
+  uint64 check_va;
+  pte_t *pte;
+  uint32 buf = 0; 
+  
+  if(argaddr(0, &va) < 0){
+    return -1;
+  }
+  if(argint(1, &num_check) < 0){
+    return -1;
+  }
+  if(argaddr(2, &dstva) < 0){
+    return -1;
+  }
+
+  for(int i = 0; i < MAXSCAN && i < num_check; i++){
+    check_va = va + i * PGSIZE; 
+    pte = walk(pagetable, check_va, 0);
+    if(pte == 0){
+      return -1;
+    }
+    if((*pte & PTE_V) == 0){
+      return -1;
+    }
+    if((*pte & PTE_U) == 0){
+      return -1;
+    }
+    if(*pte & PTE_A){
+      *pte = *pte & ~PTE_A;
+      buf |= (1 << i);  
+    }
+  }
+  
+  if(copyout(pagetable, dstva, (char *)&buf, sizeof(buf)) < 0){
+    return -1;
+  }
+
+  return 0;
+}
+#endif
+
 uint64
 sys_kill(void)
 {
   int pid;
 
-  argint(0, &pid);
+  if(argint(0, &pid) < 0)
+    return -1;
   return kill(pid);
 }
 
--- kernel/vm.c
+++ kernel/vm.c
@@ -43,7 +43,7 @@ kvmmake(void)
   // the highest virtual address in the kernel.
   kvmmap(kpgtbl, TRAMPOLINE, (uint64)trampoline, PGSIZE, PTE_R | PTE_X);
 
-  // allocate and map a kernel stack for each process.
+  // map kernel stacks
   proc_mapstacks(kpgtbl);
   
   return kpgtbl;
@@ -61,12 +61,7 @@ kvminit(void)
 void
 kvminithart()
 {
-  // wait for any previous writes to the page table memory to finish.
-  sfence_vma();
-
   w_satp(MAKE_SATP(kernel_pagetable));
-
-  // flush stale entries from the TLB.
   sfence_vma();
 }
 
@@ -208,12 +203,12 @@ uvmcreate()
 // for the very first process.
 // sz must be less than a page.
 void
-uvmfirst(pagetable_t pagetable, uchar *src, uint sz)
+uvminit(pagetable_t pagetable, uchar *src, uint sz)
 {
   char *mem;
 
   if(sz >= PGSIZE)
-    panic("uvmfirst: more than a page");
+    panic("inituvm: more than a page");
   mem = kalloc();
   memset(mem, 0, PGSIZE);
   mappages(pagetable, 0, PGSIZE, (uint64)mem, PTE_W|PTE_R|PTE_X|PTE_U);
@@ -223,7 +218,7 @@ uvmfirst(pagetable_t pagetable, uchar *src, uint sz)
 // Allocate PTEs and physical memory to grow process from oldsz to
 // newsz, which need not be page aligned.  Returns new size or 0 on error.
 uint64
-uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz, int xperm)
+uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
 {
   char *mem;
   uint64 a;
@@ -239,7 +234,7 @@ uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz, int xperm)
       return 0;
     }
     memset(mem, 0, PGSIZE);
-    if(mappages(pagetable, a, PGSIZE, (uint64)mem, PTE_R|PTE_U|xperm) != 0){
+    if(mappages(pagetable, a, PGSIZE, (uint64)mem, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
       kfree(mem);
       uvmdealloc(pagetable, a, oldsz);
       return 0;
@@ -435,5 +430,37 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return 0;
   } else {
     return -1;
+  }
+}
+
+void 
+vmprint(pagetable_t pagetable, int depth)
+{
+  if(depth == 0){
+    printf("page table %p\n", pagetable);
+  }
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      if(depth == 0){
+        printf("..%d: pte %p pa %p\n", i, pte, child);
+      }else if(depth == 1){
+        printf(".. ..%d: pte %p pa %p\n", i, pte, child);
+      }else{
+        printf(".. .. ..%d: pte %p pa %p\n", i, pte, child);
+      }
+      vmprint((pagetable_t)child, depth+1);
+    }else if(pte & PTE_V){
+      uint64 pa = PTE2PA(pte);
+      if(depth == 0){
+        printf("..%d: pte %p pa %p\n", i, pte, pa);
+      }else if(depth == 1){
+        printf(".. ..%d: pte %p pa %p\n", i, pte, pa);
+      }else{
+        printf(".. .. ..%d: pte %p pa %p\n", i, pte, pa);
+      }
+    }
   }
 }
